39094af854dab823a5e95af822a3c44c43a5b714
hbollon <hugo.bollon@proton.me>
2026-02-25 20:09:14 +0100

    chore(lint): disable html self-closing rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,7 @@
 import { createConfigForNuxt } from '@nuxt/eslint-config'
 
-export default createConfigForNuxt()
+export default createConfigForNuxt().append({
+  rules: {
+    'vue/html-self-closing': 'off',
+  },
+})
